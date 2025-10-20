5b56e5486b38471cd1c21491b6124c8e16167f7f
Eren Filiz <erenfiliz1907@gmail.com>
2025-10-20 09:54:13 -0400

    Logout Logic

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -15,10 +15,9 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     @Override
     public void execute() {
-        // TODO: implement the logic of the Logout Use Case
-        // * set the current username to null in the DAO
-        // * instantiate the `LogoutOutputData`, which needs to contain the username.
-        // * tell the presenter to prepare a success view.
+        userDataAccessObject.setCurrentUsername(null);
+        LogoutOutputData outputData = new LogoutOutputData(userDataAccessObject.getCurrentUsername());
+        logoutPresenter.prepareSuccessView(outputData);
     }
 }
 
